225eedcb7f0998fc3a4a78cacdeb4151a53bf979
unknown <lakshmimogili28@gmail.com>
2025-08-06 21:00:39 +0530

    Uploaded Sparse Arrays Solution file

---
+++ HackerRank/Week 1/Sparse Arrays.c
@@ -0,0 +1,29 @@
+//Sparse Arrays
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int n,q;
+    scanf("%d", &n);
+    char strings[1000][21];
+    for (int i=0;i<n;i++){
+        scanf("%s",strings[i]);
+    }
+    scanf ("%d",&q);
+    char queries[1000][21];
+    for (int i=0;i<q;i++){
+        scanf("%s",queries[i]);
+    }
+    for (int i=0;i<q;i++){
+        int count =0;
+        for (int j=0;j<n;j++){
+            if (strcmp(queries[i],strings[j])==0) {
+                count++;
+
+            }    
+        }
+        printf("%d\n",count);
+    }
+    return 0;
+
+
+}
